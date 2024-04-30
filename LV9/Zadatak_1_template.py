f5f4fda81390161206b371a1a61e3d714a81a915
Dombah <=>
2024-04-30 12:00:51 +0200

    Finish LV9

--- LV9/Zadatak_1_template.py
+++ LV9/Zadatak_1_template.py
@@ -6,6 +6,11 @@ from tensorflow.keras.utils import to_categorical
 from matplotlib import pyplot as plt
 
 
+#mreža ima 10 slojeva i 1,122,758 parametara
+# točnost na testnom skupu: 72.01
+
+# tocnost na testnom skupu nakon dropouta: 74.96 
+
 # ucitaj CIFAR-10 podatkovni skup
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
@@ -38,13 +43,19 @@ model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padd
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(layers.Flatten())
 model.add(layers.Dense(500, activation='relu'))
+model.add(layers.Dropout(0.3))
 model.add(layers.Dense(10, activation='softmax'))
 
 model.summary()
 
 # definiraj listu s funkcijama povratnog poziva
+
+# pri treniranju s 15 epoha se zaustavilo na 13. epohi 
 my_callbacks = [
-    keras.callbacks.TensorBoard(log_dir = 'logs/cnn',
+    keras.callbacks.EarlyStopping ( monitor ="val_loss" ,
+        patience = 5 ,
+        verbose = 1),
+    keras.callbacks.TensorBoard(log_dir = 'logs/cnn_dropout',
                                 update_freq = 100)
 ]
 
@@ -52,14 +63,23 @@ model.compile(optimizer='adam',
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
 
+
+# 74.15 na 5 epoha
+# Veliki batch size (90) tocnost: 73.24
 model.fit(X_train_n,
             y_train,
-            epochs = 40,
-            batch_size = 64,
+            epochs = 5,
+            batch_size = 10,
             callbacks = my_callbacks,
             validation_split = 0.1)
 
 
 score = model.evaluate(X_test_n, y_test, verbose=0)
 print(f'Tocnost na testnom skupu podataka: {100.0*score[1]:.2f}')
+
+#4. zad
+# 1). Kada je preveliki batch size moze doci do overfittinga, odnosno opada tocnost, kada je premala onda moze biti underfitting
+# 2). S malom stopom ucenja se model trenira duze dog s velikom stopom ucenja krace. 
+# 3). Ako izbacimo dovoljno dobar broj slojeva mozemo dobiti precizniji model. Npr ako je model pretreniran onda je to dobro. Ako izbacimo previse onda model postaje nepecizan
+# 4). Smanjuje se preciznost modela
 
